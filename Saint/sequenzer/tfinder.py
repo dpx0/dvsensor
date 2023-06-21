dffa7e082404167fbade2286c9edd17543a0924c
Saint Fischer <saint.fischer@outlook.com>
2023-06-21 13:23:20 +0200

    clean up, comments added, arparse implemented
    im happy as it is, feel free to change as you need and see fit :3

--- Saint/sequenzer/tfinder.py
+++ Saint/sequenzer/tfinder.py
@@ -1,29 +1,31 @@
 from Bio import SeqIO
 from Bio import Seq
 import csv
-import numpy as np
+import argparse
 
-#Diese Datei sucht die angegebenen Tripplets und gibt die stellen zurück. EIngabe soll eine dna/rna .fasta und die gesuchten tripplets sein
+parser = argparse.ArgumentParser(description='Process .FASTA single DNA/RNA into possible DART VADER targets') #übergabe der Argumente
+parser.add_argument('filename')
+args = parser.parse_args()
 
-
-inp = "IDH1_ref.fasta" #input soll später mit arg übergeben werden
-
+inp = args.filename
 data = SeqIO.read(inp, "fasta")
+file = data.id + ".csv" #output
+
 target = data.seq
-target = target.transcribe() #dna zu rna
-w_seq = target #working seq. I will cut this
-hit = []
-tripplet = "CCA"
+target = target.transcribe() #if dna is given: dna to rna
+w_seq = target #working seq we will cut this
+
 tripplets = ["CCA", "GCA", "UCA", "CAA"]
-lfind = -3 #jeder find wird mit lfind und 3 addiert um die stelle der seq zu finden
-file = data.id + ".csv"
-loc = []
+lfind = -3                                          #Found Index will be added to lfind and 3. for the first loop it dot need the +3 so we offset with -3. This is bc we cut the working sequenz and the cut length need to be readded to new found index
+
+
 
 towrite = [["INDEX", "LOCATION", "TRIPPLET"],]
 
-def findlocs(w_seq): #Funktion sucht nach 5UTR, ORF, 3UTR
-    loc.append(w_seq.find("AUG")) #Finde das startcodon
-    a = w_seq.find("UAA") #Finde das stoppcodon
+def findlocs(w_seq):
+    loc = []                                        #Looking for 5UTR, ORF, 3UTR
+    loc.append(w_seq.find("AUG"))                   #Find startcodon
+    a = w_seq.find("UAA")                           #Find stoppcodon
     if a < w_seq.find("UGA"):
         a = w_seq.find("UGA")
     if a < w_seq.find("UAG"):
@@ -33,12 +35,12 @@ def findlocs(w_seq): #Funktion sucht nach 5UTR, ORF, 3UTR
 
 loc = findlocs(target)
 
-def searchhits(w_seq, tripplet, lfind): #Funktion sucht eingabesequenz nach der erstgelegenen trippletstelle, gibt sie an hit ab und schneidet die seq. wiederholung bis keine seq mehr da
-    #print(len(w_seq))
-    print(w_seq)
+def searchhits(w_seq, tripplet, lfind):             #Search input for first tripplet and append index to list. cut already searched part and repeat
+    #print(len(w_seq))                              #debug stuff
+    #print(w_seq)                                   #debug stuff
     find = w_seq.find(tripplet)
     if find != -1:
-        lfind = lfind + 3 + find
+        lfind = lfind + 3 + find                     #Find out if found index is before the first start codon or before the first stop codon
         if lfind < loc[0]:
             towrite.append([lfind, "5UTR", tripplet])
         else:
@@ -46,30 +48,24 @@ def searchhits(w_seq, tripplet, lfind): #Funktion sucht eingabesequenz nach der 
                 towrite.append([lfind, "ORF", tripplet])
             else:
                 towrite.append([lfind, "3UTR", tripplet])
-        #towrite.append([lfind, loc, tripplet])
-        hit.append((lfind))
-        print(hit)
-        print(w_seq[find:(find+3)])
-        w_seq = w_seq[find+3::] #Die liste wird stückweise kleiner find weiß wo die nächste seq ist
-        #print(len(w_seq)+hit)
-        searchhits(w_seq, tripplet, lfind)
+
+
+        #print(w_seq[find:(find+3)])                #debug stuff
+
+        w_seq = w_seq[find+3::]                     #Cut searched part from the list. Biopython.find only returns index of first match in Sequence ig. Thats why we cut and add found index + lfind
+        searchhits(w_seq, tripplet, lfind)          #Recursion
 
 
 
 
-for trip in tripplets: #Jedes Angegebene Tripplet wird einmal durchlaufen
+for trip in tripplets:                              #For each definded tripplet we can run searchits
     searchhits(w_seq, trip, lfind)
     w_seq = target
     lfind = -3
 
-for index in range(len(hit)): #überprüft ob die Ergebnisse stimmen, ausgabe aller stellen, die hit gefunden hat
-    print(target[hit[index]:hit[index]+3])
 
-
-with open('sample.csv', 'w', newline="\n") as f: #das ganze als csv speichern
+with open(file, 'w', newline="\n") as f:            #Save as .csv
     mywriter = csv.writer(f, dialect='excel', delimiter=',')
     mywriter.writerows(towrite)
 
-print(loc)
-
-#np.savetxt('sample.csv',arr, fmt = '%d', delimiter=",")
+#print(loc)                                         #debug stuff
