7d4518c6ca88bb32cdea8715addeea0e963a0b90
dprib <daniel.prib@uni-bielefeld.de>
2023-08-21 06:52:03 +0200

    added ui elements to results page

--- dvsensor/build/view/pages/results_page.py
+++ dvsensor/build/view/pages/results_page.py
@@ -14,10 +14,20 @@ def update_progress(progress_bar, progress_label, step: float) -> float:
 	return progress_bar.value
 
 
-def set_status_finished(spinner, status_text):
+def set_status_finished(spinner, status_text, cancel_button, export_button):
 	spinner.visible = False
+	cancel_button.visible = False
+	export_button.visible = True
 	status_text.text = 'Analysis finished'
 	status_text.style(replace=f'color: {Colors.GREEN}')
+
+
+def set_status_cancelled(spinner, status_text, cancel_button, export_button):
+	spinner.visible = False
+	cancel_button.visible = False
+	export_button.visible =  True
+	status_text.text = 'Analysis cancelled'
+	status_text.style(replace=f'color: {Colors.RED}')
 
 
 def build(view, **kwargs) -> None:
@@ -32,11 +42,52 @@ def build(view, **kwargs) -> None:
 		# ----- top row
 		with ui.row().classes('w-full flex justify-between mt-2 mb-10 space-x-10'):
 
-			# ----- target ideogram
+			# ----- target name + ideogram
 			with ui.column().classes('grow'):
+
+				# ----- target name
 				ui.label(f'Target: {view.model.record_name} ({view.model.record_id})')\
 					.classes('text-center text-lg font-mono font-semibold')\
 					.style(f'color: {Colors.FOREGROUND}')
+
+				# min % for label: ~10%
+				len_5UTR = 261
+				len_CDS = 3633
+				len_3UTR = 6011
+				len_tot = len_5UTR + len_CDS + len_3UTR
+
+				percent_5UTR = round(len_5UTR / len_tot, 2) * 100
+				percent_CDS = round(len_CDS / len_tot, 2) * 100
+				percent_3UTR = 100.0 - (percent_5UTR + percent_CDS)
+
+				# ----- target ideogram
+				with ui.row().classes('w-full gap-0 pt-8'):
+					with ui.column().classes().style(f'width: {percent_5UTR}%'):
+
+						with ui.element('div').classes('w-full bg-stone-500'):
+							ui.label(f"""{"5'-UTR" if percent_5UTR >= 10 else '*'}""")\
+								.classes('text-center font-semibold')
+
+					with ui.column().classes().style(f'width: {percent_CDS}%'):
+						with ui.element('div').classes('w-full bg-yellow-500'):
+							ui.label(f"{'CDS' if percent_CDS >= 10 else '*'}")\
+								.classes('text-center font-semibold')
+
+					with ui.column().classes().style(f'width: {percent_3UTR}%'):
+						with ui.element('div').classes('w-full bg-indigo-500'):
+							ui.label(f"""{"3'-UTR" if percent_3UTR >= 10 else '*'}""")\
+								.classes('text-center font-semibold')
+
+				with ui.row().classes('w-full justify-center gap-x-28'):
+					ui.label(f"5'-UTR: {len_5UTR} bp")\
+						.classes('text-center text-sm font-mono') \
+						.style(f'color: {Colors.FOREGROUND}')
+					ui.label(f"CDS: {len_CDS} bp")\
+						.classes('text-center text-sm font-mono')\
+						.style(f'color: {Colors.FOREGROUND}')
+					ui.label(f"3'-UTR: {len_3UTR} bp")\
+						.classes('text-center text-sm font-mono')\
+						.style(f'color: {Colors.FOREGROUND}')
 
 			# ----- target information
 			with ui.column().classes('1/4'):
@@ -54,12 +105,15 @@ def build(view, **kwargs) -> None:
 					.style(f'color: {Colors.FOREGROUND}')
 
 			# ----- status + control elements
-			with ui.column().classes('w-1/3 flex justify-between place-content-center'):
+			status_box = ui.column().classes('w-1/3 flex justify-between place-content-center')
+			with status_box:
 				with ui.row():
 					spinner = ui.spinner(color=Colors.ACCENT, size='xl').classes()
 					status_text = ui.label(f'Analysis running...')\
 						.classes('self-center text-lg font-mono font-semibold')\
 						.style(f'color: {Colors.ACCENT}')
+
+		ui.separator().props('dark')
 
 		# ----- progress bar + label
 		with ui.grid(columns=2).classes('w-full flex space-x-4 pl-6'):
@@ -67,7 +121,7 @@ def build(view, **kwargs) -> None:
 				.classes('w-16 text-lg font-mono font-semibold')\
 				.style(f'color: {Colors.FOREGROUND}')
 			progress_bar = ui.linear_progress(show_value=False)\
-				.props('track-color=grey-1 color=green-9')\
+				.props('track-color=grey-1 color=green-9 stripe')\
 				.classes('w-0 h-4 grow self-center')
 
 		# ----- sensor table
@@ -90,9 +144,24 @@ def build(view, **kwargs) -> None:
 			ui_control_functions={
 				'add_rows': partial(add_rows, sensors_table),
 				'update_progress': partial(update_progress, progress_bar, progress_label),
-				'set_status_finished': lambda: set_status_finished(spinner, status_text)
+				'set_status_finished': lambda: set_status_finished(spinner, status_text,
+																   cancel_button, export_button),
 			})
 
-		# ui.button('CANCEL', on_click=task_control_functions['cancel_task'])
+		with status_box:
+			export_button = ui.button('export table as CSV',
+									  on_click=lambda: sensors_table.call_api_method(
+										  'exportDataAsCsv', {'suppressQuotes': True})) \
+				.props('color=indigo-10').classes('self-center')
+			export_button.visible = False
+
+			cancel_button = ui.button('cancel',
+									  on_click=lambda: (task_control_functions['cancel_task'](),
+														set_status_cancelled(spinner, status_text,
+																			 cancel_button, export_button)))\
+				.props('color=red-10').classes('self-center')
+
+
+
 
 	footer()
